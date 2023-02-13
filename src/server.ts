853340ebb40643abdf23668cf0e05279ef925fd8
bruno <bpires.siqueira@gmail.com>
2023-02-13 17:36:29 -0300

    :heavy_check_mark:

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mustache from 'mustache-express';
 import path from 'path';
+import mainRoutes from './routes/index';
 
 dotenv.config(); // DotEnv porta
 
@@ -14,5 +15,10 @@ server.engine('mustache', mustache()); // Engine
 server.use(express.static(path.join(__dirname, '../public'))); // Pasta estática onde ficam as imagens e o CSS
 
 // ROTAS
+server.use(mainRoutes);
+
+server.use((req, res) => {
+  res.send('Página não encontrada!');
+});
 
 server.listen(process.env.PORT); // Porta (4000) que está setada no meu arquivo .env
