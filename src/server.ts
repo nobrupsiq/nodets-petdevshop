6d7b78f8234b86a0f40e51ad59821c9376950be5
bruno <bpires.siqueira@gmail.com>
2023-02-13 21:01:25 -0300

    Final project 1.0:rocket:

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ server.use(express.static(path.join(__dirname, '../public'))); // Pasta estátic
 server.use(mainRoutes);
 
 server.use((req, res) => {
-  res.send('Página não encontrada!');
+  res.render('pages/404');
 });
 
 server.listen(process.env.PORT); // Porta (4000) que está setada no meu arquivo .env
